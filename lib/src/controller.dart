c4cb323f8b4bc5f2370bc7a3d10906599fe1b55f
Emil <emilzulufov.commercial@gmail.com>
2025-09-29 23:02:57 +0500

    feat: add IntExtension to provide convenient byte size conversions in SupabaseUploadController

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -229,6 +229,23 @@ class SupabaseUploadController {
   }
 }
 
+//ignore_for_file: non_constant_identifier_names
+extension IntExtension on int {
+  /// Number in bytes
+  int get B => this;
+
+  /// Number in Kilo Bytes
+  int get KB => B * 1024;
+
+  /// Number in Mega Bytes
+  int get MB => KB * 1024;
+
+  /// Number in Giga Bytes
+  int get GB => MB * 1024;
+
+  //No need to create others
+}
+
 /// {@template file_extension}
 /// Extension on [File] to check if it is a video file.
 /// {@endtemplate}
